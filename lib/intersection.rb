6ea39ecafb0670b7a75026a57e6d7df77778e34e
Junichi Ito <jit@sonicgarden.jp>
2014-04-21 07:37:38 +0900

    Break lines.

--- lib/intersection.rb
+++ lib/intersection.rb
@@ -35,7 +35,14 @@ class Intersection
     if !check_size?
       first_arr  = create_arr[0]
       second_arr = create_arr[1]
-      first_arr.include?([@xb1, @yb1]) || first_arr.include?([@xb2, @yb2]) || first_arr.include?([@xb1, @yb2]) || first_arr.include?([@xb2, @yb1]) || second_arr.include?([@xa1, @ya1]) || second_arr.include?([@xa2, @ya2]) || second_arr.include?([@xa1, @ya2]) || second_arr.include?([@xa2, @ya1])
+      first_arr.include?( [@xb1, @yb1]) ||
+        first_arr.include?([@xb2, @yb2]) ||
+        first_arr.include?([@xb1, @yb2]) ||
+        first_arr.include?([@xb2, @yb1]) ||
+        second_arr.include?([@xa1, @ya1]) ||
+        second_arr.include?([@xa2, @ya2]) ||
+        second_arr.include?([@xa1, @ya2]) ||
+        second_arr.include?([@xa2, @ya1])
     else
       false
     end
