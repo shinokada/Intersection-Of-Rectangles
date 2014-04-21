4bde4093faf10b393295a50686143ab2d7f8bf18
Junichi Ito <jit@sonicgarden.jp>
2014-04-21 09:52:20 +0900

    Refactoring spec.

--- spec/lib/rectangle_spec.rb
+++ spec/lib/rectangle_spec.rb
@@ -8,14 +8,14 @@ describe Rectangle do
   context "yes 1" do
     let(:params) { { xa1: '0.0', ya1: '0.0', xa2: '5.0', ya2: '5.0',
                      xb1: '1.0', yb1: '1.0', xb2: '4.0', yb2: '4.0' } }
-    describe "Rectangle#inside_points" do
+    describe "#inside_points" do
       specify { expect(rectangle_a.inside_points).to eq (0..5).to_a.product((0..5).to_a) }
       specify { expect(rectangle_b.inside_points).to eq (1..4).to_a.product((1..4).to_a) }
     end
-    describe "Rectangle#same_size?" do
+    describe "#same_size?" do
       specify { expect(rectangle_a.same_size?(rectangle_b)).to be_false }
     end
-    describe "#included?" do
+    describe "::included?" do
       specify { expect(Rectangle.included?(params)).to be_true }
     end
   end
@@ -23,10 +23,10 @@ describe Rectangle do
   context "yes 2" do
     let(:params) { { xa1: '0.0', ya1: '0.0', xa2: '4.0', ya2: '5.0',
                      xb1: '1.0', yb1: '1.0', xb2: '5.0', yb2: '5.0' } }
-    describe "Rectangle#same_size?" do
+    describe "#same_size?" do
       specify { expect(rectangle_a.same_size?(rectangle_b)).to be_false }
     end
-    describe "#included?" do
+    describe "::included?" do
       specify { expect(Rectangle.included?(params)).to be_true }
     end
   end
@@ -34,10 +34,10 @@ describe Rectangle do
   context "no" do
     let(:params) { { xa1: '0.0', ya1: '0.0', xa2: '4.0', ya2: '4.0',
                      xb1: '-3.0', yb1: '-5.0', xb2: '2.0', yb2: '-1.0' } }
-    describe "Rectangle#same_size?" do
+    describe "#same_size?" do
       specify { expect(rectangle_a.same_size?(rectangle_b)).to be_false }
     end
-    describe "#included?" do
+    describe "::included?" do
       specify { expect(Rectangle.included?(params)).to be_false }
     end
   end
@@ -45,10 +45,10 @@ describe Rectangle do
   context "No since they are the same size" do
     let(:params) { { xa1: '1.0', ya1: '1.0', xa2: '2.0', ya2: '3.0',
                      xb1: '2.0', yb1: '1.0', xb2: '4.0', yb2: '2.0' } }
-    describe "Rectangle#same_size?" do
+    describe "#same_size?" do
       specify { expect(rectangle_a.same_size?(rectangle_b)).to be_true }
     end
-    describe "#included?" do
+    describe "::included?" do
       specify { expect(Rectangle.included?(params)).to be_false }
     end
   end
@@ -56,7 +56,7 @@ describe Rectangle do
   context "one rectangle in another" do
     let(:params) { { xa1: '0.0', ya1: '0.0', xa2: '4.0', ya2: '4.0',
                      xb1: '-2.0', yb1: '-2.0', xb2: '6.0', yb2: '5.0' } }
-    describe "#included?" do
+    describe "::included?" do
       specify { expect(Rectangle.included?(params)).to be_true }
     end
   end
@@ -64,7 +64,7 @@ describe Rectangle do
   context "Unidentified coordinate is in another" do
     let(:params) { { xa1: '0.0', ya1: '0.0', xa2: '4.0', ya2: '4.0',
                      xb1: '2.0', yb1: '-5.0', xb2: '10.0', yb2: '2.0' } }
-    describe "#included?" do
+    describe "::included?" do
       specify { expect(Rectangle.included?(params)).to be_true }
     end
   end
